c718182aa51acc098e483737e1f50ef0293ee779
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2024-05-08 00:11:27 +0800

    feat: intial setup for TableView

---
+++ app/src/main/java/com/example/myapplication/ColumnHeader.java
@@ -0,0 +1,7 @@
+package com.example.myapplication;
+import androidx.annotation.Nullable;
+public class ColumnHeader extends Cell {
+    public ColumnHeader(@Nullable Object data) {
+        super(data);
+    }
+}
