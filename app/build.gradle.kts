bd141b8a6fd5c1a449618ee0681a18147536c400
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2024-05-18 16:41:32 +0800

    deps: add firebase google signin support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,8 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation ("de.hdodenhof:circleimageview:3.1.0")
-
+    implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.android.gms:play-services-auth:21.1.1")
 
 }
